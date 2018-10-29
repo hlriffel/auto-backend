88d3a7260fc648f064f26697b7bb0aa47c4c2534
Riffel <henleoriffel@gmail.com>
2018-10-28 21:20:21 -0300

    initial setup

--- src/main/java/br/edu/senac/auto/controller/UserController.java
+++ src/main/java/br/edu/senac/auto/controller/UserController.java
@@ -3,10 +3,7 @@ package br.edu.senac.auto.controller;
 import br.edu.senac.auto.domain.User;
 import br.edu.senac.auto.repository.generic.IGenericRepository;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -23,7 +20,7 @@ public class UserController {
     }
 
     @PostMapping
-    public void addUser(User user) {
+    public void addUser(@RequestBody User user) {
         repository.add(user);
     }
 
