2439e43af9de3ac8584b1d4fae880087c09221ce
hlriffel <henrique.riffel@philips.com>
2018-11-11 18:01:49 -0200

    Delete CaracteristicaRepository.java

--- src/main/java/br/edu/senac/auto/repository/CaracteristicaRepository.java
+++
@@ -1,7 +0,0 @@
-package br.edu.senac.auto.repository;
-import org.springframework.data.jpa.repository.JpaRepository;
-import br.edu.senac.auto.domain.Caracteristica;
-
-public interface  CaracteristicaRepository extends JpaRepository<Caracteristica, Long> {
-
-}
